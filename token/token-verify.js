353ac1c7277cd6e8ffcd576ab01b4bb63899c375
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-06-04 00:02:40 +0700

    update token

--- token/token-verify.js
+++ token/token-verify.js
@@ -1,19 +1,32 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-  
-    if (!token) {
-      return res.status(401).json({ error: 'Authorization failed' });
-    }
-  
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-      if (err) {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Authorization failed' });
+  }
+
+  const tokenWithoutBearer = token.replace('Bearer ', '');
+
+  if (!process.env.TOKEN_SECRET) {
+    return res.status(500).json({ error: 'Token secret is not defined' });
+  }
+
+  jwt.verify(tokenWithoutBearer, process.env.TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ error: 'Token expired' });
+      }
+      if (err.name === 'JsonWebTokenError') {
         return res.status(401).json({ error: 'Invalid token' });
       }
-  
-      req.user = decoded;
-      next();
-    });
-  };
-  
+      return res.status(500).json({ error: 'Failed to authenticate token' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = verifyToken;
