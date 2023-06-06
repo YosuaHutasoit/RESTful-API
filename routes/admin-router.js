9d64bbc4abd31413f016f01b38f12ef0634ee1a0
Yosua Mual Pardomuan Hutasoit <yosuahutasoit082@gmail.com>
2023-06-06 12:10:26 +0700

    update router

--- routes/admin-router.js
+++ routes/admin-router.js
@@ -1,87 +1,14 @@
-const express = require('express')
-const auth = require('../token/token-verify')
-const adminModel = require('../model/admin')
+const express = require('express');
+require('dotenv').config();
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
+const { 
+  register, 
+  login, 
+} = require('../controllers/User');
 
-  try {
-    const existingAdmin = await adminModel.findOne({ email });
-    if (existingAdmin) {
-      return res.status(409).json({ message: 'Email already registered' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const admin = new adminModel({
-      name,
-      email,
-      password: hashedPassword,
-      role: 'admin'
-    });
-
-    const newAdmin = await admin.save();
-
-    const token = jwt.sign({ userId: newAdmin._id }, process.env.TOKEN_SECRET);
-
-    res.status(201).json({ token });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const admin = await adminModel.findOne({ email });
-
-    if (!admin) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    const passwordMatch = await bcrypt.compare(password, admin.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    const token = jwt.sign({ userId: admin._id }, process.env.TOKEN_SECRET);
-
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.get('/', auth, async (req, res) => {
-  try {
-    const admins = await adminModel.find();
-    res.json(admins);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.post('/', auth, async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const admin = new adminModel({
-      name,
-      email,
-      password: hashedPassword,
-      role: 'admin'
-    });
-
-    const newAdmin = await admin.save();
-    res.status(201).json(newAdmin);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.post('/signup', register);
+router.post('/signin', login);
 
 module.exports = router;
